3de254f8ea9b1713c9cb6eaedc40b225bc601ed2
melonattacker <junkitombob@gmail.com>
2020-12-05 11:51:50 +0900

    ADD: ABC 178 c

---
+++ contests/ABC/ABC178/c.cpp
@@ -0,0 +1,28 @@
+// Ai = 0なるiとAi = 9なるiがともに存在するのは
+// 10 ** N - 9 ** N - 9 ** N - 8 ** N
+
+#include <iostream>
+
+const long long mod = 1000000007;
+
+long long powmod(long long x, long long y) {
+    long long result = 1;
+    for(int i = 0; i < y; i++) {
+        result = result * x % mod;
+    }
+    return result;
+}
+
+int main() {
+    long long n, ans;
+    std::cin >> n;
+
+    ans = powmod(10, n) - powmod(9, n) - powmod(9, n) + powmod(8, n);
+
+    ans = ans % mod;
+    ans = (ans + mod) % mod;
+
+    std::cout << ans << std::endl;
+
+    return 0;
+}
