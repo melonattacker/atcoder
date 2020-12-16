db1c9e0812f429bd7b4c459bfe47757c9cf41316
melonattacker <junkitombob@gmail.com>
2020-12-16 10:53:51 +0900

    ADD: ABC 174 a

---
+++ contests/ABC/ABC174/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+    
+    if(X >= 30) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
