d064df465863e047998b577e580c8ecff5c9b2fc
melonattacker <junkitombob@gmail.com>
2020-12-25 16:12:58 +0900

    ADD: ABC 171 a

---
+++ contests/ABC/ABC171/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char s;
+    cin >> s;
+
+    if (isupper(s))
+    {
+        cout << "A" << endl;
+    }
+    else
+    {
+        cout << "a" << endl;
+    }
+
+    return 0;
+}
