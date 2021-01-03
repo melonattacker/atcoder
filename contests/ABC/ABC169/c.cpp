437a7c70a7ca95ae1012c556289a5f768a9485fe
melonattacker <junkitombob@gmail.com>
2021-01-03 19:00:29 +0900

    ADD: ABC 169 c

---
+++ contests/ABC/ABC169/c.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unsigned long long A, B;
+    double b;
+    cin >> A >> b;
+
+    B = b * 1000;
+
+    cout << (A * B) / 1000 << endl;
+
+    return 0;
+}
