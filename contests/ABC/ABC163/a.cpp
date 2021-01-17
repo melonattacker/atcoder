946616acc7c1a34f21de4c1fb682251799f76a27
melonattacker <junkitombob@gmail.com>
2021-01-17 15:29:08 +0900

    ADD: ABC 163 a b

---
+++ contests/ABC/ABC163/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    double R;
+    cin >> R;
+
+    cout << fixed << setprecision(20) << 2 * R * M_PI << endl;
+
+    return 0;
+}
