e7ebddb544268d9fc301340e27292914f49f0039
melonattacker <junkitombob@gmail.com>
2021-03-07 21:25:12 +0900

    ADD: ABC 138 a

---
+++ contests/ABC/ABC138/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int a;
+    string s;
+    cin >> a >> s;
+
+    if(a >= 3200) cout << s << endl;
+    else cout << "red" << endl;
+
+    return 0;
+}
