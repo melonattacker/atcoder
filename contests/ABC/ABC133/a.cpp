9114226037c05dd5bcfa022899604a426357e864
melonattacker <junkitombob@gmail.com>
2021-03-24 21:58:07 +0900

    ADD: ABC 133 a

---
+++ contests/ABC/ABC133/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int N, A, B;
+    cin >> N >> A >> B;
+
+    cout << min(N*A, B) << endl;
+
+    return 0;
+}
