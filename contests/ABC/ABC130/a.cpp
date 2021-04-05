f9385efdd328180798c5d67b2cc6380191cd2d77
melonattacker <junkitombob@gmail.com>
2021-04-05 09:39:29 +0900

    ADD: ABC 130 a

---
+++ contests/ABC/ABC130/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int X, A;
+    cin >> X >> A;
+
+    if(X < A) cout << 0 << endl;
+    else cout << 10 << endl;
+
+    return 0;
+}
