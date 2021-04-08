55cbe3a3390e8509b1b2ec15560b4adb455c92d5
melonattacker <junkitombob@gmail.com>
2021-04-08 23:06:08 +0900

    ADD: ABC 128 a

---
+++ contests/ABC/ABC128/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int A, P;
+    cin >> A >> P;
+
+    cout << (A * 3 + P ) / 2 << endl;
+
+    return 0;
+}
