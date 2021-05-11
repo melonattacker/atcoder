faa37d758fd1ab054baf24a43fe9370d1e2d6548
melonattacker <junkitombob@gmail.com>
2021-05-11 09:40:12 +0900

    ADD: arihon knapsack-1

---
+++ arihon/2-3/knapsack-1.cpp
@@ -0,0 +1,31 @@
+// 個数制限なしナップサック問題
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int n, W;
+    cin >> n >> W;
+
+    vector<int> w(n);
+    vector<int> v(n);
+    vector<vector<int>> dp(n+1, vector<int>(W+1));
+
+    for(int i = 0; i < n; i++) {
+        cin >> w[i] >> v[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= W; j++) {
+            if(j < w[i]) {
+                dp[i+1][j] = dp[i][j];
+            } else {
+                dp[i+1][j] = max(dp[i][j], dp[i+1][j-w[i]]+v[i]);
+            }
+        }
+    }
+
+    cout << dp[n][W] << endl;
+
+    return 0;
+}
