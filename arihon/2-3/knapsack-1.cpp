5173826698cd642d8c24613c6853c3daa4f87552
melonattacker <junkitombob@gmail.com>
2021-05-12 23:28:47 +0900

    ADD: コメントの追加

--- arihon/2-3/knapsack-1.cpp
+++ arihon/2-3/knapsack-1.cpp
@@ -18,6 +18,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j <= W; j++) {
             if(j < w[i]) {
+                // dp[i+1][j]: i番目までの品物から重さの総和がj以下となるように選んだ時の、価値の総和の最大値
                 dp[i+1][j] = dp[i][j];
             } else {
                 dp[i+1][j] = max(dp[i][j], dp[i+1][j-w[i]]+v[i]);
