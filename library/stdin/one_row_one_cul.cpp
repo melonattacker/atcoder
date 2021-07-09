b55d98a61d166c027b7c3852e3ec54b367a59ad3
melonattacker <junkitombob@gmail.com>
2021-07-09 20:42:02 +0900

    ADD: dp c

--- library/stdin/one_row_one_cul.cpp
+++ library/stdin/one_row_one_cul.cpp
@@ -3,6 +3,8 @@ using namespace std;
 typedef long long ll;
 // const int INF = 100000000;
 const long long INF = 1LL << 60;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
 int main() {
     int N;
