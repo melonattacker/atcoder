330a28afa51ed883dc66dae37d6adfa720338d3b
melonattacker <junkitombob@gmail.com>
2021-07-21 09:42:07 +0900

    ADD: ABC 125 a

---
+++ contests/ABC/ABC125/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+// const int INF = 100000000;
+const long long INF = 1LL << 60;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main() {
+    int A, B, T;
+    cin >> A >> B >> T;
+
+    cout << T / A * B << endl;
+    return 0;
+}
