8681afd5b3cadfb7f931f883326a6bce5f7af5ae
melonattacker <junkitombob@gmail.com>
2021-12-25 11:18:24 +0900

    ADD: マクロの追加

--- library/stdin/one_row_one_cul.cpp
+++ library/stdin/one_row_one_cul.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
 using namespace std;
-typedef long long ll;
-// const int INF = 1 << 30;
-const long long INF = 1LL << 60;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
 int main() {
     int N;
