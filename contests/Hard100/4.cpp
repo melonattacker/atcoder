fab5b02dcc2ba8e30d4287bb5aaa986e58cdf4fd
melonattacker <junkitombob@gmail.com>
2022-01-03 21:02:59 +0900

    ADD: Hard 100 4

---
+++ contests/Hard100/4.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+ll f(ll n, ll x) {
+    return n/x+1;
+}
+
+int main() {
+    ll a, b, x;
+    cin >> a >> b >> x;
+
+    ll ans = f(b, x) - f(a-1, x);
+
+    cout << ans << endl;
+
+    return 0;
+}
