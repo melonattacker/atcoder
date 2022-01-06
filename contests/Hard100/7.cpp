3405eebd6dd8c45764ab7f825e2690ab5c07c6d9
melonattacker <junkitombob@gmail.com>
2022-01-06 10:51:25 +0900

    ADD: Hard 100 7

--- contests/Hard100/7.cpp
+++ contests/Hard100/7.cpp
@@ -11,12 +11,30 @@ template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } 
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
 int main() {
-    int N;
-    cin >> N;
+    int N, M;
+    cin >> N >> M;
 
+    // 計算量は O(MlogM + NlogN)
     V<int> A(N);
-
     rep(i, 0, N) cin >> A[i];
+    sort(A.begin(),A.end()); // Aを小さい順に並べる
+    V<pair<int, int>> v(M);
+    rep(i, 0, M){
+        int B, C; cin >> B >> C;
+        v[i] = {C,B};
+    }
+    sort(v.rbegin(), v.rend()); // Cを大きい順に並べる
 
+    int l = 0;
+    for(auto p: v) {
+        int cnt = p.second;
+        while(cnt-- && A[l] < p.first && l < N) {
+            A[l++] = p.first; // Cを大きい順に取り出して最大B個書き換える
+        }
+    }
+
+    ll ans = 0;
+    rep(i, 0, N) ans += 1LL * A[i];
+    cout << ans << endl;
     return 0;
 }
