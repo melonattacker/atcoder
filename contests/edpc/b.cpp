395e4a5b06694e7525b275215318b0cef3b080b2
melonattacker <junkitombob@gmail.com>
2022-02-23 14:20:53 +0900

    ADD: edpc b

---
+++ contests/edpc/b.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    V<int> h(N);
+    V<int> dp(N, inf);
+    rep(i, 0, N) cin >> h[i];
+
+    dp[0] = 0;
+    dp[1] = abs(h[1]-h[0]);
+    // cout << dp[1] << endl;
+    rep(i, 2, N) {
+        // cout << i << endl;
+        rep(j, 1, K+1) {
+            if(i-j>=0) {
+                chmin(dp[i], dp[i-j]+abs(h[i-j]-h[i]));
+            }
+        }
+        cout << endl;
+    }
+    cout << dp[N-1] << endl;
+
+    return 0;
+}
