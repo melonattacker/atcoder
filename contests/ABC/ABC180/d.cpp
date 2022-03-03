277e996b9c0ff465803710175c0c777e5f682e89
melonattacker <junkitombob@gmail.com>
2022-03-03 11:24:06 +0900

    ADD: ABC 180 d

---
+++ contests/ABC/ABC180/d.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    ll x, y, a, b;
+    cin >> x >> y >> a >> b;
+    ll ans = 0;
+    while((double) a*x <= 2e18 && a*x <= b+x && a*x < y) {
+        x*=a;
+        ans++;
+    }
+    ans += (y-1-x)/b;
+    cout << ans << endl;
+    return 0;
+}
