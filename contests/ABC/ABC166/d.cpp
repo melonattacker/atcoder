a2a6f44afd6472e8763411d9d47d4d445930f240
melonattacker <junkitombob@gmail.com>
2022-03-08 12:16:19 +0900

    ADD: ABC 166 d

---
+++ contests/ABC/ABC166/d.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    ll X;
+    cin >> X;
+
+    unordered_map<ll, ll> mp;
+    rep(x, -1000, 1000) mp[1LL * x * x * x * x * x] = x;
+
+    for(auto p: mp) {
+        ll B5 = p.first;
+        ll B = p.second;
+
+        if(mp.count(X+B5)) {
+            ll A = mp[X+B5];
+            cout << A << " " << B << endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
