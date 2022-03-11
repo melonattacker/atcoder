34ce8600ff84cea6a072a78c02479b9bddb706d2
melonattacker <junkitombob@gmail.com>
2022-03-11 10:58:21 +0900

    ADD: ABC 165 d

---
+++ contests/ABC/ABC165/d.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    ll A, B, N;
+    cin >> A >> B >> N;
+
+    ll ans = 0;
+    chmax(ans, (A*N/B)-A*(N/B));
+    if(B-1 <= N) chmax(ans, (A*(B-1)/B)-A*((B-1)/B));
+    cout << ans << endl;
+
+    return 0;
+}
