99dc2a49f693f4d50e93007dc63c43e46035fd5e
melonattacker <junkitombob@gmail.com>
2022-03-31 10:23:26 +0900

    ADD: Hard 100 29

---
+++ contests/Hard100/29.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    int N;
+    cin >> N;
+
+    V<ll> A(N);
+    rep(i, 0, N) cin >> A[i];
+    sort(all(A));
+
+    ll sum = 0;
+    int t = 0;
+    rep(i, 0, N) {
+        if(2*sum < A[i]) {
+            t = i;
+        }
+        sum += A[i];
+    }
+
+    cout << N - t << endl;
+    return 0;
+}
