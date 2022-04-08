1c4ec92c66071078aadfcbd5d6164cfc41a097cc
melonattacker <junkitombob@gmail.com>
2022-04-08 10:32:41 +0900

    ADD: Hard 100 30

---
+++ contests/Hard100/30.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    V<int> A(N);
+    int g = 0, maxi = 0;
+    rep(i, 0, N) {
+        cin >> A[i];
+        g = gcd(g, A[i]);
+        maxi = max(maxi, A[i]);
+    }
+
+    if(K % g == 0 && K <= maxi) {
+        cout << "POSSIBLE" << endl;
+    } else {
+        cout << "IMPOSSIBLE" << endl;
+    }
+
+    return 0;
+}
