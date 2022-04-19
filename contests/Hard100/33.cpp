77030c2cb2756159211ce32b470c2fd397e4d6ab
melonattacker <junkitombob@gmail.com>
2022-04-19 12:17:27 +0900

    ADD: Hard 100 33

---
+++ contests/Hard100/33.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    int N;
+    cin >> N;
+
+    V<int> A(N);
+    rep(i, 0, N) cin >> A[i];
+
+    ll x2 = 0;
+    rep(i, 0, N) {
+        if(i % 2 == 0) x2 += A[i];
+        else x2 -= A[i];
+    }
+
+    vector<int> ans(N);
+    ans[0] = x2/2;
+    rep(i, 0, N) {
+        ans[i+1] = A[i] - ans[i]; 
+    }
+
+    rep(i, 0, N) ans[i] *= 2;
+    rep(i, 0, N) cout << ans[i] << " ";
+    cout << endl;
+
+    return 0;
+}
