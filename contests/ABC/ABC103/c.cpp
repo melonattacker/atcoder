d609f17855eb5a52cce58b145c86689bde6d4318
melonattacker <junkitombob@gmail.com>
2022-04-25 23:11:42 +0900

    ADD: ABC 103 c

---
+++ contests/ABC/ABC103/c.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    int N;
+    cin >> N;
+
+    V<int> a(N);
+    rep(i, 0, N) cin >> a[i];
+
+    int ans = 0;
+    rep(i, 0, N) {
+        ans += a[i]-1;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
