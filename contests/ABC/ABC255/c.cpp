fac09dc2511e5692091b2443bd90e0f2ac12af46
melonattacker <junkitombob@gmail.com>
2022-06-13 12:32:56 +0900

    ADD: ABC 255 c

---
+++ contests/ABC/ABC255/c.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using Graph = vector<vector<int>>;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+    ll X, A, D, N;
+    cin >> X >> A >> D >> N;
+
+    if(D < 0) {
+        ll fi = A+D*(N-1);
+        A = fi;
+        D *= -1; 
+    }
+
+    ll st = 0, fi = (N-1);
+    while(st <= fi) {
+        ll mi = (st+fi)/2;
+        if((A+D*mi)<X) st = mi+1;
+        else fi = mi-1;
+    }
+    ll res = 8e18;
+    for(ll i=max(0ll, st-5);i<=min((N-1),st+5);i++) {
+        res = min(abs(A+D*i-X), res);
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
